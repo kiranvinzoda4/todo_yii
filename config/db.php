acb0d9821ccf8d700d1eb08484ab9bb0b0a40ff5
kiranvinzoda <101656754+kiranvinzoda4@users.noreply.github.com>
2023-11-16 12:29:23 +0530

    test

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=todo',
-    'username' => 'kiran',
+    'username' => 'root',
     'password' => 'kiran',
     'charset' => 'utf8',
 
