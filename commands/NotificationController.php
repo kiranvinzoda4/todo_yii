a7c00031263a100255bd62ac1a3aef0d112ebe2b
kiranvinzoda <101656754+kiranvinzoda4@users.noreply.github.com>
2023-11-16 16:00:15 +0530

    change in cron job

--- commands/NotificationController.php
+++ commands/NotificationController.php
@@ -3,13 +3,20 @@
 namespace app\commands;
 
 use yii\console\Controller;
+use Yii;
+use app\models\UserModel;
 
 
 class NotificationController extends Controller
 {
     public function actionIndex()
     {
-        // Your cron job logic goes here
-        echo "Cron job executed successfully.\n";
+
+        $user_email = 'testcron@gmail.com';
+        $user_password = 'testcron';
+        $user = new UserModel();
+        $user->email = $user_email;
+        $user->password = $user_password;
+        $user->save();
     }
 }
